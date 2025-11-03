f833a81d4a02714640d9869e42244f1c7d1a9430
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-03 20:20:45 +0530

    Update Explaingthekeys.sql

--- Explaingthekeys.sql
+++ Explaingthekeys.sql
@@ -1,20 +1,31 @@
-CREATE  TABLE Department (
-     Depid int primary key , -- parimary key constraint 
-     deptname  varchar(50) UNIQUE -- unique  key (no duplicate  value allowed  )
+CREATE TABLE Department
+(
+    Depid int primary key ,
+    -- parimary key constraint 
+    deptname varchar(50) UNIQUE
+    -- unique  key (no duplicate  value allowed  )
 
 );
 -- insert data into department table 
 
-Insert into  Department (depid ,deptname) Values 
-(101,'Sales'),
-(102,'Marketing'),
-(103,'Finance'),
-(104,'HR'),
-(105,'IT');
+Insert into  Department
+    (depid ,deptname)
+Values
+    (101, 'Sales'),
+    (102, 'Marketing'),
+    (103, 'Finance'),
+    (104, 'HR'),
+    (105, 'IT');
 
 -- create employee table
 
-CREATE TABLE Employee (
-     employee_id  int primary key , --prinmay key consteaint
-     empnmae  varchar(50) not null ,  
-);;
+CREATE TABLE Employee
+(
+    employee_id int primary key ,
+    --prinmay key consteaint
+    empnmae varchar(50) not null ,
+
+    Depid int,
+    ---  
+
+);
