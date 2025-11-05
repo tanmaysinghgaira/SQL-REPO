80fffdffa274a0dd6b7b21aede5779ccc418dc6d
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-05 21:20:50 +0530

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -3,7 +3,7 @@ Create table Coustomer(
     CustomerName varchar (50)
 );
 --step 2 insert the data into coustomer table
-insert into Coustomer (Customer_id , CoustomerName) VALUES
+insert into Coustomer (Customer_id , CustomerName) VALUES
 (101,'Tanmay'),
 (102, 'rohan'),
 (103,'sachin'),
@@ -14,6 +14,21 @@ insert into Coustomer (Customer_id , CoustomerName) VALUES
 Create table Orders(
     OrderId int primary key,
     OrderDate date,
-    Customer_id int,
+     Customer_id int, -- 
+    Product varchar(50)
 
 );
+Insert into Orders(OrderId, OrderDate, Customer_id,Product )Values
+(1,'2025-01-10',101,'Laptop'),
+(2,2025-03-15,102,'Mobile'),
+(3,'2025-04-20',103,'Tablet'),
+(4,2025-05-25,104,'Desktop'); --Notice customer id  105 dosent exist in coustomer table
+
+
+select Coustomer.Customer_id, Coustomer.CustomerName,Orders.Product
+from Coustomer
+inner join Orders
+on Coustomer.Customer_id = Orders.Customer_id;
+
+
+
