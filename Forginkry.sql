3b5f2f92628503d1a38287bc05e6ed9ebedd8972
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-07 19:21:02 +0530

    Update Forginkry.sql

--- Forginkry.sql
+++ Forginkry.sql
@@ -15,7 +15,16 @@ CREATE TABLE  Ord(
     CONSTRAINT FK_Orders_Child FOREIGN KEY (Customer) REFERENCES Child_tabel(Customer)
 );
 
-INSERT INTO CHILD_TABEL(COUSTOMER, NAME,CITY)VALUES
-(1,'TANMAY,UTTRAKHAND'),
+INSERT INTO Child_tabel (Customer, Name, City )VALUES
+(1,'TANMAY','UTTRAKHAND'),
 (2, 'RAM','NANITAL'),
 (3,'SHAM','DEHTADUN');
+
+INSERT into Ord(Order_Id,Order_Date,Customer,Product)
+VALUES
+(1,'2024-01-10',1,'LAPTOP'),
+(2,'2024-03-15',2,'MOBILE'),
+(3,'2024-04-20',3,'TABLET'),
+(4,'2024-05-25',2,'DESKTOP');
+
+SELECT *FROM Ord;
