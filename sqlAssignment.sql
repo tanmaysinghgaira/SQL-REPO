be4d1e50c460f34a6b3d9c5b4dfbb9249240b91f
Tanmay gaira <tanmaygaira911@gmail.com>
2025-11-16 18:32:53 +0530

    Update sqlAssignment.sql

---
+++ sqlAssignment.sql
@@ -0,0 +1,19 @@
+-- DDL Commands (Create, Alter, Drop)
+--1
+Create TABLE Student(
+    StudentId int primary key,
+    StudentName varchar(50),
+    Age int,
+    department varchar(50)
+);
+
+--2 Create tabel Course
+DROP TABLE IF EXISTS Course; -- Drop table if exists as samr name table may exist
+Create table Course(
+    CourseID int primary key,
+    Coursename varchar(50),
+    Department varchar(50)
+);
+ SELECT * FROM Course;
+--add eamil column to student tabel
+Alter table Student add Email varchar(50);
